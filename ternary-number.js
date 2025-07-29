5f4d653ba8e99b8c7788adb4631ee65387615286
asadul <asadul33489686@gmail.com>
2025-07-29 12:00:52 +0600

    ternary operator

---
+++ ternary-number.js
@@ -0,0 +1,22 @@
+ let num1=5;
+ let num2=8;
+// result;
+// if(num1>num2){
+//     result=num1*2
+// }else{
+//     result= num1+num2
+// }
+
+
+
+// console.log("Result is:", result)
+
+
+
+
+
+
+
+// ternary 
+let result= (num1>num2) ? num1*2 : num1+num2;
+console.log("result is:", result);

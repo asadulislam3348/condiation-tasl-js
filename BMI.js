0ac6081ff3fc5089f8223950ea7f742c5885b9a8
asadul <asadul33489686@gmail.com>
2025-07-29 09:54:35 +0600

    add grade calculator

--- BMI.js
+++ BMI.js
@@ -5,14 +5,14 @@ let weight=65;
 let hight=1.85;
 let bmi=weight/(hight)^2;
 
-console.log('your bmi is:' +bmi);
+console.log(" your bmi is:" +bmi);
 
 if(bmi<18.5){
-    console.log('you are underweight');
-}else if (bmi>=18.5 && bmi<=24.9){
-        console.log('you are normal');
-    }else if (bmi >= 25 && bmi<=29.9){
-        console.log('you are overweight');
-    } else{
-    console.log('You are obese');
+    console.log(" you are underweight.");
+}else if(bmi >= 18.5 && bmi <=24.9){
+    console.log("you are normal.");
+}else if(bmi >=25 && bmi <=29.9){
+    console.log("you are overweight.");
+}else{
+    console.log("you are obese.");
 }
---
+++ Grade-Calculator.js
@@ -0,0 +1,13 @@
+let score=55;
+
+if(score>=90 && score<=100){
+    console.log("your grade is: A");
+}else if(score>=80 && score<=89){
+console.log("your grade is: B");
+}else if(score>=70 && score<=79){
+console.log("your grade is: C");
+}else if(score>=60 && score<=69){
+console.log("your grade is: D");
+}else if(score>=0 && score<=59){
+console.log("your grade is: F");
+}
